446d21431f9b246b6966425e72b8f8244f80aaaf
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-20 19:39:09 -0700

    - Domain is renamed to Entities - Added Repository project - Created RepositoryContext.cs in the Repository project.

--- Entities/Models/Employee.cs
+++ Entities/Models/Employee.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Domain.Models;
+namespace Entities.Models;
 
 public class Employee
 {
