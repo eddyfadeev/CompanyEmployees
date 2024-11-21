c2838272259b83d8be795bbff04c165ef1607451
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-21 10:21:51 -0700

    Added IRepositoryBase interface

---
+++ Contracts/Repository/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Contracts.Repository;
+
+public interface IRepositoryBase<TEntity>
+{
+    IQueryable<TEntity> FindAll(bool trackChanges);
+    IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression, bool trackChanges);
+    void Create(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+}
