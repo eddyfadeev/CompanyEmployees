d16d05a2b7a4a3cb0db0dda4fad581726be39464
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-23 00:25:42 -0700

    feat(api): add CompanyNotFoundException class
    - Added CompanyNotFoundException class to handle exceptions when company is not found
    - Updated CompaniesController to handle GET request for specific company
    - Imported necessary namespaces in CompaniesController

---
+++ Entities/Exceptions/CompanyNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities.Exceptions;
+
+public class CompanyNotFoundException : NotFoundException 
+{
+    public CompanyNotFoundException(Guid id) 
+        : base($"The company with id: { id } does not exist in the database.")
+    {
+    }
+    
+    public CompanyNotFoundException() 
+        : base("The company does not exist in the database.")
+    {
+    }
+}
