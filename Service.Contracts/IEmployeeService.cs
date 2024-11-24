db635b710e4615d8984ad838575cf3b410aae28f
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-24 16:31:01 -0700

    feat(employee): added GetEmployees method
    - Added the GetEmployees method to the EmployeeService class to
    retrieve a list of employee data based on the company ID and tracking
    changes flag.
    - Updated the IEmployeeService interface to include the GetEmployees method signature.

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -1,5 +1,8 @@
-﻿namespace Service.Contracts;
+﻿using Shared.DTO;
+
+namespace Service.Contracts;
 
 public interface IEmployeeService
 {
+    IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
 }
--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,6 +1,9 @@
 ﻿using Contracts.Logging;
 using Contracts.Repository;
+using Entities.Exceptions;
 using Service.Contracts;
+using Shared.DTO;
+using Shared.Extensions;
 
 namespace Service;
 
@@ -13,5 +16,18 @@ internal sealed class EmployeeService : IEmployeeService
     {
         _repository = repository;
         _logger = logger;
+    }
+
+    public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+    {
+        var company = _repository.Company.GetCompany(companyId, trackChanges);
+        if (company is null)
+        {
+            throw new CompanyNotFoundException(companyId);
+        }
+
+        var employees = _repository.Employee.GetEmployees(companyId, trackChanges);
+        
+        return employees.Select(e => e.MapToDto());
     }
 }
