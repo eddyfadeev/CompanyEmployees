fac6d6e0a3e94ca5e744bd912f370ad27551b77b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-24 20:30:50 -0700

    feat(service.tests): add InMemoryDatabaseProvider class
    Added a new file InMemoryDatabaseProvider.cs to the Service.Tests project. This class contains a static method to create an in-memory database context with given companies and employees. The method adds the entities to the context, saves changes, and returns the context.

---
+++ Service.Tests/InMemoryDatabaseProvider.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace Service.Tests;
+
+internal static class InMemoryDatabaseProvider
+{
+    public static async Task<RepositoryContext> CreateDatabaseContext(IEnumerable<Company> companies, IEnumerable<Employee> employees)
+    { 
+        var context = GetInMemoryDbContext();
+
+        await context.Companies.AddRangeAsync(companies);
+        await context.Employees.AddRangeAsync(employees);
+        await context.SaveChangesAsync();
+        
+        return context;
+    }
+    
+    private static RepositoryContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseInMemoryDatabase(
+                databaseName: $"TestDb_{Guid.NewGuid().ToString()}")
+            .Options;
+
+        return new RepositoryContext(options);
+    }
+}
