2ecd6c510c338aa3f12227f0d3c60fc320564af5
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-24 20:25:48 -0700

    feat(entities): added EmployeeNotFoundException class

---
+++ Entities/Exceptions/EmployeeNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Exceptions;
+
+public class EmployeeNotFoundException : Exception
+{
+    public EmployeeNotFoundException(Guid employeeId) 
+        : base($"Employee with id: {employeeId} does not exist in database.") {}
+}
