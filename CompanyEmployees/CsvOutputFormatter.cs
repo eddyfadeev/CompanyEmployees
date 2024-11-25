88a2b9a9d67ed4dd93c7123c57efab06e1e10137
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-25 12:38:04 -0700

    feat(csv): add support for formatting EmployeeDto in CSV output
    - Updated CsvOutputFormatter.cs to include formatting for EmployeeDto
    - Added switch case for handling EmployeeDto and IEnumerable<EmployeeDto>
    - Updated FormatCsv method to handle EmployeeDto properties
    - Added EmployeeDto fields to CSV output format

--- CompanyEmployees/CsvOutputFormatter.cs
+++ CompanyEmployees/CsvOutputFormatter.cs
@@ -17,7 +17,9 @@ public class CsvOutputFormatter : TextOutputFormatter
     protected override bool CanWriteType(Type? type)
     {
         if (typeof(CompanyDto).IsAssignableFrom(type) ||
-            typeof(IEnumerable<CompanyDto>).IsAssignableFrom(type))
+            typeof(IEnumerable<CompanyDto>).IsAssignableFrom(type) ||
+            typeof(EmployeeDto).IsAssignableFrom(type) ||
+            typeof(IEnumerable<EmployeeDto>).IsAssignableFrom(type))
         {
             return base.CanWriteType(type);
         }
@@ -30,23 +32,34 @@ public class CsvOutputFormatter : TextOutputFormatter
         var response = context.HttpContext.Response;
         var buffer = new StringBuilder();
 
-        if (context.Object is IEnumerable<CompanyDto> companyDtos)
+        switch (context.Object)
         {
-            foreach (var company in companyDtos)
-            {
-                FormatCsv(buffer, company);
-            }
-        }
-        else
-        {
-            FormatCsv(buffer, (CompanyDto)context.Object);
+            case CompanyDto companyDto:
+                FormatCsv(buffer, companyDto);
+                break;
+            case IEnumerable<CompanyDto> companies:
+                foreach (var company in companies)
+                {
+                    FormatCsv(buffer, company);
+                }
+                break;
+            case EmployeeDto employee:
+                FormatCsv(buffer, employee);
+                break;
+            case IEnumerable<EmployeeDto> employees:
+                foreach (var employee in employees)
+                {
+                    FormatCsv(buffer, employee);
+                }
+                break;
         }
         
         await response.WriteAsync(buffer.ToString());
     }
 
-    private static void FormatCsv(StringBuilder buffer, CompanyDto company)
-    {
+    private static void FormatCsv(StringBuilder buffer, CompanyDto company) =>
         buffer.AppendLine($"{company.Id}, \"{company.Name}\", \"{company.FullAddress}\"");
-    }
+    
+    private static void FormatCsv(StringBuilder buffer, EmployeeDto employee) =>
+        buffer.AppendLine($"{employee.Id}, \"{employee.Name}\", \"{employee.Age}\", \"{employee.Position}\"");
 }
