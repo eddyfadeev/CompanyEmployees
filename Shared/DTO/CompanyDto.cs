e1afafc3c2751a5ac51e48391ee7d4cd608ebde2
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-25 17:16:21 -0700

    Refactored CompanyDto.cs to use auto-properties

--- Shared/DTO/CompanyDto.cs
+++ Shared/DTO/CompanyDto.cs
@@ -3,23 +3,7 @@
 [Serializable]
 public record CompanyDto
 {
-    private readonly Guid id;
-    private readonly string name;
-    private readonly string address;
-    private readonly string country;
-
-    public CompanyDto(Guid id, string name, string address, string country)
-    {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.country = country;
-    }
-    
-    public Guid Id => id;
-    public string Name => name;
-    public string FullAddress => string.Join(' ', address, country);
-
-    public void Deconstruct(out Guid id, out string name, out string address, out string country) =>
-        (id, name, address, country) = (this.id, this.name, this.address, this.country);
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string FullAddress { get; init; }
 }
