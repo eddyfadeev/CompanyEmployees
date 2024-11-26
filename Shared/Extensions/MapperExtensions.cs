df5ca4060a7005063e45f5a2aefb7b9dacee6652
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-26 00:06:17 -0700

    feat: Changed methods signatures MapToCompanyForCreationDto and EmployeeForCreationDto methods
    - Changed signatures to improve readability of the code

--- Shared/Extensions/MapperExtensions.cs
+++ Shared/Extensions/MapperExtensions.cs
@@ -5,15 +5,25 @@ namespace Shared.Extensions;
 
 public static class MapperExtensions
 {
-    public static CompanyDto MapToDto(this Company company) =>
+    public static CompanyDto MapToCompanyDto(this Company company) =>
         new ()
         {
             Id = company.Id,
             Name = company.Name ?? string.Empty,
             FullAddress = string.Join(' ', company.Address, company.Country)
         };
+
+    public static CompanyForCreationDto MapToCompanyForCreationDto(
+        this Company company, params IEnumerable<EmployeeForCreationDto> optionalEmployees) =>
+        new
+        (
+            Name: company.Name ?? string.Empty,
+            Address: company.Address ?? string.Empty,
+            Country: company.Country ?? string.Empty,
+            Employees: optionalEmployees
+        );
     
-    public static EmployeeDto MapToDto(this Employee employee) =>
+    public static EmployeeDto MapToEmployeeDto(this Employee employee) =>
         new ()
         {
             Id = employee.Id,
@@ -21,13 +31,22 @@ public static class MapperExtensions
             Age = employee.Age,
             Position = employee.Position ?? string.Empty
         };
+    
+    public static EmployeeForCreationDto MapToEmployeeForCreationDto(this Employee employee) =>
+        new 
+        (
+            Name: employee.Name ?? string.Empty,
+            Age: employee.Age,
+            Position: employee.Position ?? string.Empty
+        );
 
     public static Company MapToEntity(this CompanyForCreationDto company) =>
         new ()
         {
             Name = company.Name,
             Address = company.Address,
-            Country = company.Country
+            Country = company.Country,
+            Employees = company.Employees.Select(e => e.MapToEntity()).ToList()
         };
 
     public static Employee MapToEntity(this EmployeeForCreationDto employee) =>
