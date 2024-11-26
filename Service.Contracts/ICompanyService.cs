756b4041e8fb6bcc783571bb7a0d06a3bf6b1d46
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-26 13:34:57 -0700

    feat(Service.Contracts): Add method CreateCompanyCollection to ICompanyService.cs interface.

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -8,4 +8,6 @@ public interface ICompanyService
     CompanyDto GetCompany(Guid id, bool trackChanges);
     CompanyDto CreateCompany(CompanyForCreationDto company);
     IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid>? ids, bool trackChanges);
+    (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection
+        (IEnumerable<CompanyForCreationDto>? companyCollection);
 }
