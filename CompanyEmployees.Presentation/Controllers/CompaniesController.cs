3422df3475d63b78f99cf3e060278a0bb24d0b0e
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-26 01:31:52 -0700

    feat(controllers): Added new endpoint to retrieve a collection of companies by IDs in CompaniesController

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -41,4 +41,12 @@ public class CompaniesController : ControllerBase
 
         return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
     }
+
+    [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+    public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+    {
+        var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+
+        return Ok(companies);
+    }
 }
