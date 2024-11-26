16cdd50f9906cb2ff6171dbf794827a59e628cf7
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-25 17:19:13 -0700

    feat(ServiceManagerTests.CompanyTests): added tests for CreateCompany and related methods
    - Added tests to ensure the `CreateCompany` method returns a non-null result when a company is created.
    - Updated existing tests to cover scenarios where the full address is provided in the DTO, ensuring it maps correctly to the returned DTO.
    
    fix(ServiceManagerTests.CompanyTests): handle missing test data properly
    
    - Added test cases to ensure that the `GetAllCompanies_ReturnsEmptyList_WhenNoEntriesInDb` method works as expected when there are no entries in the database.

--- Service.Tests/ServiceManagerTests.CompanyTests.cs
+++ Service.Tests/ServiceManagerTests.CompanyTests.cs
@@ -1,5 +1,8 @@
 ﻿using Entities.Exceptions;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 using Shared.DTO;
+using Shared.Extensions;
 
 namespace Service.Tests;
 
@@ -8,21 +11,34 @@ public partial class ServiceManagerTests
     private IEnumerable<CompanyDto> _companies;
     
     [Test]
-    public async Task GetAllCompanies_ReturnsAllCompanies()
+    public async Task GetAllCompanies_ReturnsAllCompanies_WhenDbEntriesExist()
     {
         var expected = _companies.ToList();
 
-        var result = 
-            await Task.Run(() => _companyService.CompanyService.GetAllCompanies(trackChanges: false));
+        var result = await Task.Run(() => 
+            _companyService.CompanyService.GetAllCompanies(trackChanges: false));
         
         Assert.That(result, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public async Task GetAllCompanies_ReturnsEmptyList_WhenNoEntriesInDb()
+    {
+        var entities = await _context.Companies.ToListAsync();
+        _context.RemoveRange(entities);
+        await _context.SaveChangesAsync();
+        
+        var result = await Task.Run(() => 
+            _companyService.CompanyService.GetAllCompanies(trackChanges: false));
+        
+        Assert.That(result, Is.Empty);
     }
 
     [Test]
     public async Task GetCompany_ReturnsCorrectCompany()
     {
         var expected = _companies.FirstOrDefault();
-        Assert.That(expected, Is.Not.Null);
+        Assert.That(expected, Is.Not.Null, "Error getting test company from Db");
 
         var result = await Task.Run(() => 
             _companyService.CompanyService.GetCompany(expected.Id, trackChanges: false));
@@ -37,4 +53,48 @@ public partial class ServiceManagerTests
         Assert.Throws<CompanyNotFoundException>(() => 
             _companyService.CompanyService.GetCompany(wrongId, trackChanges: false));
     }
+    
+    
+    [Test]
+    public async Task CreateCompany_ReturnsNotNull_WhenCompanyCreated()
+    {
+        var dtoToCreate = new CompanyForCreationDto("Name", "Address", "Country");
+        
+        var result = _companyService.CompanyService.CreateCompany(dtoToCreate);
+        
+        Assert.That(result, Is.Not.Null);
+    }
+
+    [Test]
+    public async Task CreateCompany_ReturnsDtoWithCorrectName_WhenCompanyCreated()
+    {
+        var testCompany = new Company
+        {
+            Name = "Create Test"
+        };
+        var expectedDto = testCompany.MapToDto();
+        var dtoToCreate = new CompanyForCreationDto(testCompany.Name, Address: string.Empty, Country: string.Empty);
+        
+        var result = _companyService.CompanyService.CreateCompany(dtoToCreate);
+        
+        Assert.That(result.Name, Is.EqualTo(expectedDto.Name));
+    }
+    
+    [Test]
+    public async Task CreateCompany_ReturnsDtoWithCorrectAddress_WhenCompanyCreated()
+    {
+        var testCompany = new Company
+        {
+            Address = "123 Test Street, T6W 1T7, Ab",
+            Country = "Canada"
+        };
+        var expectedDto = testCompany.MapToDto();
+        var dtoToCreate = new CompanyForCreationDto(Name: string.Empty, testCompany.Address, testCompany.Country);
+        
+        var result = _companyService.CompanyService.CreateCompany(dtoToCreate);
+        
+        Assert.That(result.FullAddress, Is.EqualTo(expectedDto.FullAddress));
+    }
+    
+    
 }
