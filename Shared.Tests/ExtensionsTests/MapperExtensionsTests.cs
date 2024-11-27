60a71af6c972491216690220f6647ac9c703b00a
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-27 01:54:47 -0700

    Update Company_UpdateEntity tests in MapperExtensionsTests.cs to ensure correct entity is returned when passed valid, null, empty, or unchanged values for company name, address, country, and employees.

--- Shared.Tests/ExtensionsTests/MapperExtensionsTests.cs
+++ Shared.Tests/ExtensionsTests/MapperExtensionsTests.cs
@@ -7,6 +7,7 @@ namespace Shared.Tests.ExtensionsTests;
 public class MapperExtensionsTests
 {
     private Employee _expectedEmployeeEntity;
+    private Company _expectedCompanyEntity;
 
     [SetUp]
     public void Setup()
@@ -18,8 +19,19 @@ public class MapperExtensionsTests
             Age = 30,
             Position = "Developer"
         };
+        
+        _expectedCompanyEntity = new Company
+        {
+            Id = Guid.NewGuid(),
+            Name = "ABC Company",
+            Address = "123 Street, City",
+            Country = "USA",
+            Employees = [ _expectedEmployeeEntity ]
+        };
     }
 
+    #region MapToEmployeeForUpdateDto Tests
+    
     [Test]
     public void MapToEmployeeForUpdateDto_ShouldReturnCorrectDto_WhenInputIsValid()
     {
@@ -79,8 +91,11 @@ public class MapperExtensionsTests
         Assert.That(result, Is.EqualTo(expected));
     }
 
+    #endregion
+    
+    #region Update Employee Entity Tests
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedDtoIsValid()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedDtoIsValid()
     {
         var testEmployeeEntity = new Employee
         {
@@ -101,7 +116,7 @@ public class MapperExtensionsTests
     }
     
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedNameIsNull()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedNameIsNull()
     {
         var testEmployeeEntity = new Employee
         {
@@ -121,7 +136,7 @@ public class MapperExtensionsTests
     }
     
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedPositionIsNull()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedPositionIsNull()
     {
         var testEmployeeEntity = new Employee
         {
@@ -141,7 +156,7 @@ public class MapperExtensionsTests
     }
     
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedNameIsEmpty()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedNameIsEmpty()
     {
         var testEmployeeEntity = new Employee
         {
@@ -161,7 +176,7 @@ public class MapperExtensionsTests
     }
     
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedPositionIsEmpty()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedPositionIsEmpty()
     {
         var testEmployeeEntity = new Employee
         {
@@ -181,7 +196,7 @@ public class MapperExtensionsTests
     }
     
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntity_WhenAgeIsZero()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntity_WhenAgeIsZero()
     {
         var testEmployeeEntity = new Employee
         {
@@ -201,7 +216,7 @@ public class MapperExtensionsTests
     }
     
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntityWithNotChangedAge_WhenAgeIsNegative()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntityWithNotChangedAge_WhenAgeIsNegative()
     {
         var testEmployeeEntity = new Employee
         {
@@ -221,7 +236,7 @@ public class MapperExtensionsTests
     }
     
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntityWithNotChangedAge_WhenAgeIsAbove199()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntityWithNotChangedAge_WhenAgeIsAbove199()
     {
         var testEmployeeEntity = new Employee
         {
@@ -241,7 +256,7 @@ public class MapperExtensionsTests
     }
     
     [Test]
-    public void UpdateEntity_ShouldReturnUpdatedEntity_WhenAgeIs199()
+    public void Employee_UpdateEntity_ShouldReturnUpdatedEntity_WhenAgeIs199()
     {
         var testEmployeeEntity = new Employee
         {
@@ -259,4 +274,291 @@ public class MapperExtensionsTests
         
         Assert.That(result, Is.EqualTo(_expectedEmployeeEntity));
     }
+    
+    #endregion
+
+    #region Update Company Entity Tests
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedDtoIsValid()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            "UpdatedName", 
+            "UpdatedAddress", 
+            "UpdatedCountry",
+            Employees:
+            [
+                new EmployeeForCreationDto(Name: "Test1", Age: 1, Position: "Test1"), 
+                new EmployeeForCreationDto(Name: "Test2", Age: 2, Position: "Test2")
+            ]
+        );
+        
+        _expectedCompanyEntity.Name = testCompanyDto.Name;
+        _expectedCompanyEntity.Address = testCompanyDto.Address;
+        _expectedCompanyEntity.Country = testCompanyDto.Country;
+        _expectedCompanyEntity.Employees = testCompanyDto.Employees.Select(e => e.MapToEntity()).ToList();
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedNameIsNull_NameUnchanged()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            Name: null, 
+            "UpdatedAddress", 
+            "UpdatedCountry",
+            Employees:
+            [
+                new EmployeeForCreationDto(Name: "Test1", Age: 1, Position: "Test1"), 
+                new EmployeeForCreationDto(Name: "Test2", Age: 2, Position: "Test2")
+            ]
+        );
+        
+        _expectedCompanyEntity.Address = testCompanyDto.Address;
+        _expectedCompanyEntity.Country = testCompanyDto.Country;
+        _expectedCompanyEntity.Employees = testCompanyDto.Employees.Select(e => e.MapToEntity()).ToList();
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedAddressIsNull_AddressUnchanged()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            "UpdatedName", 
+            Address: null, 
+            "UpdatedCountry",
+            Employees:
+            [
+                new EmployeeForCreationDto(Name: "Test1", Age: 1, Position: "Test1"), 
+                new EmployeeForCreationDto(Name: "Test2", Age: 2, Position: "Test2")
+            ]
+        );
+        
+        _expectedCompanyEntity.Name = testCompanyDto.Name;
+        _expectedCompanyEntity.Country = testCompanyDto.Country;
+        _expectedCompanyEntity.Employees = testCompanyDto.Employees.Select(e => e.MapToEntity()).ToList();
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedCountryIsNull_CountryUnchanged()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            "UpdatedName", 
+            "UpdatedAddress", 
+            Country: null,
+            Employees:
+            [
+                new EmployeeForCreationDto(Name: "Test1", Age: 1, Position: "Test1"), 
+                new EmployeeForCreationDto(Name: "Test2", Age: 2, Position: "Test2")
+            ]
+        );
+        
+        _expectedCompanyEntity.Name = testCompanyDto.Name;
+        _expectedCompanyEntity.Address = testCompanyDto.Address;
+        _expectedCompanyEntity.Employees = testCompanyDto.Employees.Select(e => e.MapToEntity()).ToList();
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedEmployeesIsNull_EmployeesUnchanged()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            "UpdatedName", 
+            "UpdatedAddress", 
+            "UpdatedCountry",
+            Employees: null
+        );
+        
+        _expectedCompanyEntity.Name = testCompanyDto.Name;
+        _expectedCompanyEntity.Address = testCompanyDto.Address;
+        _expectedCompanyEntity.Country = testCompanyDto.Country;
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedNameIsEmpty_NameUnchanged()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            Name: string.Empty, 
+            "UpdatedAddress", 
+            "UpdatedCountry",
+            Employees:
+            [
+                new EmployeeForCreationDto(Name: "Test1", Age: 1, Position: "Test1"), 
+                new EmployeeForCreationDto(Name: "Test2", Age: 2, Position: "Test2")
+            ]
+        );
+        
+        _expectedCompanyEntity.Address = testCompanyDto.Address;
+        _expectedCompanyEntity.Country = testCompanyDto.Country;
+        _expectedCompanyEntity.Employees = testCompanyDto.Employees.Select(e => e.MapToEntity()).ToList();
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedAddressIsEmpty_AddressUnchanged()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            "UpdatedName", 
+            Address: string.Empty, 
+            "UpdatedCountry",
+            Employees:
+            [
+                new EmployeeForCreationDto(Name: "Test1", Age: 1, Position: "Test1"), 
+                new EmployeeForCreationDto(Name: "Test2", Age: 2, Position: "Test2")
+            ]
+        );
+        
+        _expectedCompanyEntity.Name = testCompanyDto.Name;
+        _expectedCompanyEntity.Country = testCompanyDto.Country;
+        _expectedCompanyEntity.Employees = testCompanyDto.Employees.Select(e => e.MapToEntity()).ToList();
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedCountryIsEmpty_CountryUnchanged()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            "UpdatedName", 
+            "UpdatedAddress", 
+            Country: string.Empty,
+            Employees:
+            [
+                new EmployeeForCreationDto(Name: "Test1", Age: 1, Position: "Test1"), 
+                new EmployeeForCreationDto(Name: "Test2", Age: 2, Position: "Test2")
+            ]
+        );
+        
+        _expectedCompanyEntity.Name = testCompanyDto.Name;
+        _expectedCompanyEntity.Address = testCompanyDto.Address;
+        _expectedCompanyEntity.Employees = testCompanyDto.Employees.Select(e => e.MapToEntity()).ToList();
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+    
+    [Test]
+    public void Company_UpdateEntity_ShouldReturnUpdatedEntity_WhenPassedEmployeesIsEmpty_EmployeesUnchanged()
+    {
+        var testCompanyEntity = new Company
+        {
+            Id = _expectedCompanyEntity.Id,
+            Name = _expectedCompanyEntity.Name,
+            Address = _expectedCompanyEntity.Address,
+            Country = _expectedCompanyEntity.Country,
+            Employees = _expectedCompanyEntity.Employees
+        };
+        
+        var testCompanyDto = new CompanyForUpdateDto(
+            "UpdatedName", 
+            "UpdatedAddress", 
+            "UpdatedCountry",
+            Employees: []
+        );
+        
+        _expectedCompanyEntity.Name = testCompanyDto.Name;
+        _expectedCompanyEntity.Address = testCompanyDto.Address;
+        _expectedCompanyEntity.Country = testCompanyDto.Country;
+
+        var result = testCompanyEntity.UpdateEntity(testCompanyDto);
+        
+        Assert.That(result, Is.EqualTo(_expectedCompanyEntity));
+    }
+
+    #endregion
 }
