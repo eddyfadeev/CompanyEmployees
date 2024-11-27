20afaae55895b70492e4912e999df411af9cd9c5
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-26 21:25:44 -0700

    feat(shared.tests): add Shared.Tests.csproj, MapperExtensionsTests.cs, and EmployeeForUpdateDto.cs - Added Shared.Tests.csproj with necessary project references and packages - Created MapperExtensionsTests.cs to test mapping extensions - Added EmployeeForUpdateDto class for updating employee records.

---
+++ Shared.Tests/ExtensionsTests/MapperExtensionsTests.cs
@@ -0,0 +1,81 @@
+﻿using Entities.Models;
+using Shared.DTO;
+using Shared.Extensions;
+
+namespace Shared.Tests.ExtensionsTests;
+
+public class MapperExtensionsTests
+{
+    private Employee _employee;
+
+    [SetUp]
+    public void Setup()
+    {
+        _employee = new Employee
+        {
+            Id = Guid.NewGuid(),
+            Name = "John Doe",
+            Age = 30,
+            Position = "Developer"
+        };
+    }
+
+    [Test]
+    public void MapToEmployeeForUpdateDto_ShouldReturnCorrectDto_WhenInputIsValid()
+    {
+        var expected = new EmployeeForUpdateDto(
+            _employee.Name, _employee.Age, _employee.Position);
+
+        var result = _employee.MapToEmployeeForUpdateDto();
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void MapToEmployeeForUpdateDto_ShouldReturnCorrectDto_WhenNameIsEmpty()
+    {
+        _employee.Name = string.Empty;
+        var expected = new EmployeeForUpdateDto(
+            _employee.Name, _employee.Age, _employee.Position);
+
+        var result = _employee.MapToEmployeeForUpdateDto();
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void MapToEmployeeForUpdateDto_ShouldReturnCorrectDto_WhenNameIsNull()
+    {
+        _employee.Name = null;
+        var expected = new EmployeeForUpdateDto(
+            string.Empty, _employee.Age, _employee.Position);
+
+        var result = _employee.MapToEmployeeForUpdateDto();
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void MapToEmployeeForUpdateDto_ShouldReturnCorrectDto_WhenPositionIsEmpty()
+    {
+        _employee.Position = string.Empty;
+        var expected = new EmployeeForUpdateDto(
+            _employee.Name, _employee.Age, _employee.Position);
+
+        var result = _employee.MapToEmployeeForUpdateDto();
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void MapToEmployeeForUpdateDto_ShouldReturnCorrectDto_WhenPositionIsNull()
+    {
+        _employee.Position = null;
+        var expected = new EmployeeForUpdateDto(
+            _employee.Name, _employee.Age, string.Empty);
+
+        var result = _employee.MapToEmployeeForUpdateDto();
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
