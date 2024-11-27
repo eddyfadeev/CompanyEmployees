129ee8f53a196e55add90342707e5438a455f86e
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-27 11:32:22 -0700

    feat(company): add method to update company details
    - Added a new method `UpdateCompany` in `CompanyService.cs` to update company details based on the provided `companyId` and `companyForUpdate` parameters. This method also includes a check for company existence and throws a `CompanyNotFoundException` if the company is not found.

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -101,4 +101,16 @@ internal sealed class CompanyService : ICompanyService
         _repository.Company.DeleteCompany(company);
         _repository.Save();
     }
+
+    public void UpdateCompany(Guid companyId, CompanyForUpdateDto companyForUpdate, bool trackChanges)
+    {
+        var company = _repository.Company.GetCompany(companyId, trackChanges);
+        if (company is null)
+        {
+            throw new CompanyNotFoundException(companyId);
+        }
+
+        company.UpdateEntity(companyForUpdate);
+        _repository.Save();
+    }
 }
