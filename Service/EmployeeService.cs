669bbde79493212d79486c801231943ab2776fc3
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-27 14:55:13 -0700

    feat(employee): add methods to get and save employee for patching
    Added two new methods to EmployeeService.cs: GetEmployeeForPatch to retrieve the employee and company from the repository for patching, and SaveChangesForPatch to update the employee entity with the patch data and save the changes.

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts.Repository;
 using Entities.Exceptions;
+using Entities.Models;
 using Service.Contracts;
 using Shared.DTO;
 using Shared.Extensions;
@@ -94,6 +95,32 @@ internal sealed class EmployeeService : IEmployeeService
         }
 
         employeeToUpdate.UpdateEntity(employee);
+        _repository.Save();
+    }
+    
+    public (EmployeeForUpdateDto employeeToPatch, Employee employeeEntity) 
+        GetEmployeeForPatch(Guid companyId, Guid employeeId, bool trackCompanyChanges, bool trackEmployeeChanges)
+    {
+        var company = _repository.Company.GetCompany(companyId, trackCompanyChanges);
+        if (company is null)
+        {
+            throw new CompanyNotFoundException(companyId);
+        }
+
+        var employeeEntity = _repository.Employee.GetEmployee(companyId, employeeId, trackEmployeeChanges);
+        if (employeeEntity is null)
+        {
+            throw new EmployeeNotFoundException(employeeId);
+        }
+
+        var employeeToPatch = employeeEntity.MapToEmployeeForUpdateDto();
+
+        return (employeeToPatch, employeeEntity);
+    }
+
+    public void SaveChangesForPatch(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity)
+    {
+        employeeEntity.UpdateEntity(employeeToPatch);
         _repository.Save();
     }
 }
