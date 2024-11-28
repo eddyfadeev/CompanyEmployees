2800504f5846308b4f8a21e7f6ca176d0ea234d2
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-27 20:02:25 -0700

    Add CompanyForManipulationDto abstract record with properties Name, Address, Country, and Employees, along with validation attributes for Name and Address.

---
+++ Shared/DTO/CompanyForManipulationDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTO;
+
+public abstract record CompanyForManipulationDto
+{
+    private readonly IEnumerable<EmployeeForCreationDto>? _employees;
+    private readonly string? _country;
+    
+    [Required(ErrorMessage = "Company name is required.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the name is 60 characters.")]
+    public string Name { get; init; } 
+    
+    [Required(ErrorMessage = "Company address is required.")]
+    [MaxLength(200, ErrorMessage = "Maximum length is 200 characters.")]
+    public string Address { get; init; }
+
+
+    public string? Country
+    {
+        get => _country;
+        init => _country = value ?? string.Empty;
+    }
+
+    public IEnumerable<EmployeeForCreationDto>? Employees
+    {
+        get => _employees;
+        init => _employees = value ?? [];
+    }
+}
