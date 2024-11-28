30e883096edaf64a93ef9cf0071efca88a29a1bb
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-27 19:55:41 -0700

    Refactor EmployeeForCreationDto and EmployeeForUpdateDto to inherit from EmployeeForManipulationDto.

--- Shared/DTO/EmployeeForCreationDto.cs
+++ Shared/DTO/EmployeeForCreationDto.cs
@@ -1,18 +1,3 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace Shared.DTO;
 
-namespace Shared.DTO;
-
-public record EmployeeForCreationDto
-{
-    [Required(ErrorMessage = "Employee name is required.")]
-    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
-    public string Name { get; init; }
-    
-    [Required(ErrorMessage = "Age is required.")]
-    [Range(1, 199, ErrorMessage = "Age must be between 1 and 199.")]
-    public int Age { get; init; }
-    
-    [Required(ErrorMessage = "Position is required.")]
-    [MaxLength(20, ErrorMessage = "Maximum length for the position is 20 characters.")]
-    public string Position { get; init; }
-}
+public record EmployeeForCreationDto : EmployeeForManipulationDto;
