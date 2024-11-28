207488adaf2d096f7e008d9df47c6d468bbbe45e
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-27 19:55:27 -0700

    feat: Add EmployeeForManipulationDto class with properties for Name, Age, and Position, including validation attributes for each property.

---
+++ Shared/DTO/EmployeeForManipulationDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTO;
+
+public abstract record EmployeeForManipulationDto
+{
+    [Required(ErrorMessage = "Employee name is required.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string Name { get; init; }
+    
+    [Required(ErrorMessage = "Age is required.")]
+    [Range(1, 199, ErrorMessage = "Age must be between 1 and 199.")]
+    public int Age { get; init; }
+    
+    [Required(ErrorMessage = "Position is required.")]
+    [MaxLength(20, ErrorMessage = "Maximum length for the position is 20 characters.")]
+    public string Position { get; init; }
+}
