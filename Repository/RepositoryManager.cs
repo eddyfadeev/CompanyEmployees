2d7c3ef9a02b7ae321bf0bb08d79a12bfb3547b6
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-27 21:15:26 -0700

    feat(repository): refactor Save method to be asynchronous in RepositoryManager.cs and IRepositoryManager.cs.

--- Repository/RepositoryManager.cs
+++ Repository/RepositoryManager.cs
@@ -25,6 +25,6 @@ public class RepositoryManager : IRepositoryManager
     public IEmployeeRepository Employee => 
         _employeeRepository.Value;
 
-    public void Save() => 
-        _repositoryContext.SaveChanges();
+    public async Task SaveAsync() => 
+        await _repositoryContext.SaveChangesAsync();
 }
