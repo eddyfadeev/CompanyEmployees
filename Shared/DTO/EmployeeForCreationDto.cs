b3b1a8403dfebf956a48a0de7225fae3c8823bb7
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-27 19:09:23 -0700

    feat(dto): Add validation attributes to EmployeeForCreationDto properties.

--- Shared/DTO/EmployeeForCreationDto.cs
+++ Shared/DTO/EmployeeForCreationDto.cs
@@ -1,3 +1,18 @@
-﻿namespace Shared.DTO;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record EmployeeForCreationDto(string Name, int Age, string Position);
+namespace Shared.DTO;
+
+public record EmployeeForCreationDto
+{
+    [Required(ErrorMessage = "Employee name is required.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string Name { get; init; }
+    
+    [Required(ErrorMessage = "Age is required.")]
+    [Range(1, 199, ErrorMessage = "Age must be between 1 and 199.")]
+    public int Age { get; init; }
+    
+    [Required(ErrorMessage = "Position is required.")]
+    [MaxLength(20, ErrorMessage = "Maximum length for the position is 20 characters.")]
+    public string Position { get; init; }
+}
