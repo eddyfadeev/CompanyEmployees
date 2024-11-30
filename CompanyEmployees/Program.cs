f92a1c4126fb9ca2e0d9f88296fb3af7d9e833d9
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-30 12:41:44 -0700

    Add ValidationFilterAttribute to ConfigureServices method in Program.cs.

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,6 +1,7 @@
 using CompanyEmployees;
 using CompanyEmployees.Extensions;
 using CompanyEmployees.Presentation;
+using CompanyEmployees.Presentation.ActionFilters;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
@@ -23,6 +24,8 @@ builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 builder.Services.Configure<ApiBehaviorOptions>(options => 
     options.SuppressModelStateInvalidFilter = true);
+
+builder.Services.AddScoped<ValidationFilterAttribute>();
 
 builder.Services.AddControllers(config =>
     {
