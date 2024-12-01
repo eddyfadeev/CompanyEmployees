9d8f0fa489d4a970fc3acc0ecb5144820c54401a
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-30 18:40:32 -0700

    test(employee-parameters): add unit tests for page size validation in EmployeeParameters class

---
+++ Shared.Tests/RequestFeatures/EmployeeParametersTests.cs
@@ -0,0 +1,66 @@
+﻿using Shared.RequestFeatures;
+
+namespace Shared.Tests.RequestFeatures;
+
+public class EmployeeParametersTests
+{
+    [Test]
+    public void RequestParameters_PageSizeDefaultsToOne_WhenSettingNegativeNum()
+    {
+        const int expected = 1;
+        var result = new EmployeeParameters
+        {
+            PageSize = -10
+        };
+
+        Assert.That(result.PageSize, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void RequestParameters_PageSizeDefaultsToOne_WhenSettingToZero()
+    {
+        const int expected = 1;
+        var result = new EmployeeParameters
+        {
+            PageSize = 0
+        };
+
+        Assert.That(result.PageSize, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void RequestParameters_PageSizeIsOne_WhenSettingToOne()
+    {
+        const int expected = 1;
+        var result = new EmployeeParameters
+        {
+            PageSize = expected
+        };
+
+        Assert.That(result.PageSize, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void RequestParameters_PageSizeDefaultsTo50_WhenSettingAbove50()
+    {
+        const int expected = 50;
+        var result = new EmployeeParameters
+        {
+            PageSize = 51
+        };
+
+        Assert.That(result.PageSize, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void RequestParameters_PageSizeIs50_WhenSetting50()
+    {
+        const int expected = 50;
+        var result = new EmployeeParameters
+        {
+            PageSize = expected
+        };
+
+        Assert.That(result.PageSize, Is.EqualTo(expected));
+    }
+}
