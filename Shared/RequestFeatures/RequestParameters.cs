84b0a29ad92fc48a4afe56f1107846679eecc143
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-30 18:40:10 -0700

    feat(shared): add RequestParameters class for handling page number and page size in API requests.

---
+++ Shared/RequestFeatures/RequestParameters.cs
@@ -0,0 +1,30 @@
+﻿namespace Shared.RequestFeatures;
+
+public abstract class RequestParameters
+{
+    private const int MinPageSize = 1;
+    private const int MaxPageSize = 50;
+
+    public int PageNumber { get; set; } = 1;
+    
+    private int _pageSize = 10;
+    public int PageSize
+    {
+        get => _pageSize;
+        set
+        {
+            switch (value)
+            {
+                case < MinPageSize:
+                    _pageSize = MinPageSize;
+                    return;
+                case > MaxPageSize:
+                    _pageSize = MaxPageSize;
+                    return;
+                default:
+                    _pageSize = value;
+                    break;
+            }
+        }
+    }
+}
