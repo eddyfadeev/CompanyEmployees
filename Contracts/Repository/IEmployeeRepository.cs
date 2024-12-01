54f812cba4811686c9672507e47eb7e283addab7
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-30 19:10:51 -0700

    feat(employee): add EmployeeParameters parameter to GetEmployees method, improving filtering capabilities.

--- Contracts/Repository/IEmployeeRepository.cs
+++ Contracts/Repository/IEmployeeRepository.cs
@@ -1,10 +1,11 @@
 ﻿using Entities.Models;
+using Shared.RequestFeatures;
 
 namespace Contracts.Repository;
 
 public interface IEmployeeRepository
 {
-    Task<IEnumerable<Employee>> GetEmployees(Guid companyId, bool trackChanges);
+    Task<IEnumerable<Employee>> GetEmployees(Guid companyId, EmployeeParameters employeeParameters ,bool trackChanges);
     Task<Employee?> GetEmployee(Guid companyId, Guid employeeId, bool trackChanges);
     void CreateEmployeeForCompany(Guid companyId, Employee employee);
     void DeleteEmployeeForCompany(Employee employee);
