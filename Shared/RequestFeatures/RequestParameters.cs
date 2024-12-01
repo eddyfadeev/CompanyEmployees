2948fa61c5e905391af8911347bbe66b20211198
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-30 19:09:36 -0700

    feat(RequestParameters): add validation for PageNumber setter to prevent values less than 1, setting it to 1 if less than MinPageNumber.

--- Shared/RequestFeatures/RequestParameters.cs
+++ Shared/RequestFeatures/RequestParameters.cs
@@ -2,11 +2,18 @@
 
 public abstract class RequestParameters
 {
+    private const int MinPageNumber = 1;
+    
     private const int MinPageSize = 1;
     private const int MaxPageSize = 50;
 
-    public int PageNumber { get; set; } = 1;
-    
+    private int _pageNumber = 1;
+    public int PageNumber
+    {
+        get => _pageNumber;
+        set => _pageNumber = value < MinPageNumber ? MinPageNumber : value;
+    }
+
     private int _pageSize = 10;
     public int PageSize
     {
