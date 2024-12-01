047dcaa975aa6e5042139a9502708f016e0e3574
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-30 18:09:11 -0700

    fix: update UpdateEmployeeForCompany method signature in IEmployeeService

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -9,9 +9,8 @@ public interface IEmployeeService
     Task<EmployeeDto> GetEmployee(Guid companyId, Guid employeeId, bool trackChanges);
     Task<EmployeeDto> CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employee, bool trackChanges);
     Task DeleteEmployeeForCompany(Guid companyId, Guid employeeId, bool trackChanges);
-    Task UpdateEmployeeForCompany(Guid companyId, Guid employeeId, EmployeeForUpdateDto employee, 
-        bool compTrackChanges, bool empTrackChanges);
+    Task UpdateEmployeeForCompany(Guid companyId, Guid employeeId, EmployeeForUpdateDto? employee, bool trackChanges);
     Task<(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity)> 
-        GetEmployeeForPatch(Guid companyId, Guid employeeId, bool trackCompanyChanges, bool trackEmployeeChanges);
+        GetEmployeeForPatch(Guid companyId, Guid employeeId, bool trackChanges);
     Task SaveChangesForPatch(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity);
 }
