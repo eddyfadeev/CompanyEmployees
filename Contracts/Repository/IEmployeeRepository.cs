60641a244a4f4f4cb752193cc721dac0221c1b39
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-30 20:27:10 -0700

    Refactor IEmployeeRepository.cs to return a PagedList of employees when getting employees based on parameters, instead of just IEnumerable.

--- Contracts/Repository/IEmployeeRepository.cs
+++ Contracts/Repository/IEmployeeRepository.cs
@@ -5,7 +5,7 @@ namespace Contracts.Repository;
 
 public interface IEmployeeRepository
 {
-    Task<IEnumerable<Employee>> GetEmployees(Guid companyId, EmployeeParameters employeeParameters ,bool trackChanges);
+    Task<PagedList<Employee>> GetEmployees(Guid companyId, EmployeeParameters employeeParameters ,bool trackChanges);
     Task<Employee?> GetEmployee(Guid companyId, Guid employeeId, bool trackChanges);
     void CreateEmployeeForCompany(Guid companyId, Employee employee);
     void DeleteEmployeeForCompany(Employee employee);
