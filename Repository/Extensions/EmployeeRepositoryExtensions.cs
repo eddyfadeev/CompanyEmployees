4c2126284e29e11fcf57ef4180317bcbc42d2400
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-01 18:03:55 -0700

    feat(repository): Add dynamic sorting functionality to EmployeeRepositoryExtensions
    - Added method Sort to enable dynamic sorting of employees based on orderByQuery parameter
    - Implemented logic to handle empty or invalid orderByQuery values
    - Utilized OrderQueryBuilder to create dynamic sorting expressions

---
+++ Repository/Extensions/EmployeeRepositoryExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Dynamic.Core;
+using Entities.Models;
+using Repository.Utility;
+
+namespace Repository.Extensions;
+
+public static class EmployeeRepositoryExtensions
+{
+    public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge, uint maxAge) =>
+        employees.Where(e => e.Age >= minAge && e.Age <= maxAge);
+
+    public static IQueryable<Employee> SearchByName(this IQueryable<Employee> employees, string searchItem) =>
+        string.IsNullOrWhiteSpace(searchItem) 
+            ? employees 
+            : employees.Where(e => 
+                e.Name != null && e.Name.Contains(searchItem.Trim().ToLower())
+                );
+    
+    public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQuery)
+    {
+        if (string.IsNullOrWhiteSpace(orderByQuery))
+        {
+            return employees.OrderBy(e => e.Name);
+        }
+
+        var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQuery);
+        
+        if (string.IsNullOrWhiteSpace(orderQuery))
+        {
+            return employees.OrderBy(e => e.Name);
+        }
+
+        return employees.OrderBy(orderQuery);
+    }
+}
