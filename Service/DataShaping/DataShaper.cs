ba5eef84d8847ec375d3cd860b01d7aeeb7c3777
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-02 13:04:22 -0700

    feat(dataShaper): implement data shaping functionality in DataShaper class
    Added a new file DataShaper.cs containing the DataShaper<T> class with methods to shape data for entities based on specified fields.

---
+++ Service/DataShaping/DataShaper.cs
@@ -0,0 +1,85 @@
+﻿using System.Dynamic;
+using System.Reflection;
+using Contracts;
+
+namespace Service.DataShaping;
+
+public class DataShaper<T> : IDataShaper<T>
+    where T : class
+{
+    public PropertyInfo[] Properties { get; set; }
+
+    public DataShaper()
+    {
+        Properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+    }
+    
+    public IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString)
+    {
+        var requiredProperties = GetRequiredProperties(fieldsString);
+
+        return FetchData(entities, requiredProperties);
+    }
+
+    public ExpandoObject ShapeData(T entity, string fieldsString)
+    {
+        var requiredProperties = GetRequiredProperties(fieldsString);
+        
+        return FetchDataForEntity(entity, requiredProperties);
+    }
+
+    private List<PropertyInfo> GetRequiredProperties(string fieldsString)
+    {
+        var requiredProperties = new List<PropertyInfo>();
+
+        if (!string.IsNullOrWhiteSpace(fieldsString))
+        {
+            var fields = fieldsString.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var field in fields)
+            {
+                var property = Array.Find(Properties, prop => 
+                        prop.Name.Equals(field.Trim(), StringComparison.InvariantCultureIgnoreCase)); 
+
+                if (property is null)
+                {
+                    continue;
+                }
+
+                requiredProperties.Add(property);
+            }
+        }
+        else
+        {
+            requiredProperties = Properties.ToList();
+        }
+
+        return requiredProperties;
+    }
+
+    private static List<ExpandoObject> FetchData(IEnumerable<T> entities, IEnumerable<PropertyInfo> requiredProperties)
+    {
+        var shapedData = new List<ExpandoObject>();
+
+        foreach (var entity in entities)
+        {
+            var shapedObject = FetchDataForEntity(entity, requiredProperties);
+            shapedData.Add(shapedObject);
+        }
+
+        return shapedData;
+    }
+
+    private static ExpandoObject FetchDataForEntity(T entity, IEnumerable<PropertyInfo> requiredProperties)
+    {
+        var shapedObject = new ExpandoObject();
+
+        foreach (var prop in requiredProperties)
+        {
+            var objectPropertyValue = prop.GetValue(entity);
+            shapedObject.TryAdd(prop.Name, objectPropertyValue);
+        }
+
+        return shapedObject;
+    }
+}
