1995f49ae301483a53fed4218370fc0c495b2d2d
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-01 17:13:52 -0700

    feat(repository): add RepositoryEmployeeExtensions.cs with methods FilterEmployees and SearchByName to filter and search employees based on age and name, respectively.

---
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+
+namespace Repository.Extensions;
+
+public static class RepositoryEmployeeExtensions
+{
+    public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge, uint maxAge) =>
+        employees.Where(e => e.Age >= minAge && e.Age <= maxAge);
+
+    public static IQueryable<Employee> SearchByName(this IQueryable<Employee> employees, string searchItem) =>
+        string.IsNullOrWhiteSpace(searchItem) 
+            ? employees 
+            : employees.Where(e => 
+                e.Name != null && e.Name.Contains(searchItem.Trim().ToLower())
+                );
+}
