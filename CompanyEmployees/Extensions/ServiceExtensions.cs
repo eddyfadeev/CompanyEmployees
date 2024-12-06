1c4853273a21f373078d7626e69c3ea641f1ff91
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-05 17:57:54 -0700

    feat: Add ConfigureIdentity method to ServiceExtensions.cs file for configuring identity with password requirements and user unique email validation.

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Contracts.Logging;
 using Contracts.Repository;
+using Entities.Models;
 using LoggerService;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Repository;
 using Service;
@@ -42,4 +44,17 @@ public static class ServiceExtensions
 
     public static void ConfigureResponseCaching(this IServiceCollection services) =>
         services.AddResponseCaching();
+    
+    public static void ConfigureIdentity(this IServiceCollection services) =>
+        services.AddIdentity<User, IdentityRole>(o =>
+        {
+            o.Password.RequireDigit = true;
+            o.Password.RequireLowercase = false;
+            o.Password.RequireUppercase = false;
+            o.Password.RequireNonAlphanumeric = false;
+            o.Password.RequiredLength = 5;
+            o.User.RequireUniqueEmail = true;
+        })
+        .AddEntityFrameworkStores<RepositoryContext>()
+        .AddDefaultTokenProviders();
 }
