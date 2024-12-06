35d4df67a3cd96badcdd1e495730327979e5013a
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-05 17:59:50 -0700

    feat: add AuthenticationController with RegisterUser method for user registration, including validation and error handling.

---
+++ CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using CompanyEmployees.Presentation.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DTO;
+
+namespace CompanyEmployees.Presentation.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public AuthenticationController(IServiceManager serviceManager) =>
+        _serviceManager = serviceManager;
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto user)
+    {
+        var result = await _serviceManager.AuthenticationService.RegisterUser(user);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        return StatusCode(201);
+    }
+}
