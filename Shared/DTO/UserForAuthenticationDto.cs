ec7dee410b8133ae923a1fba162f344f729314cf
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-05 19:40:41 -0700

    feat(shared): add UserForAuthenticationDto class with Username and Password properties, with validation attributes.

---
+++ Shared/DTO/UserForAuthenticationDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTO;
+
+public record UserForAuthenticationDto
+{
+    [Required(ErrorMessage = "User name is required!")]
+    public string? Username { get; init; }
+    [Required(ErrorMessage = "Password is required!")]
+    public string? Password { get; init; }
+}
