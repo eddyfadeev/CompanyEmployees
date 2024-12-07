e261589369ebcd016b743d9be28ecd31e17e2254
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-07 16:28:58 -0700

    feat(controller): add TokenController with Refresh method for token refresh functionality. The controller handles token refresh requests and returns the updated token DTO.

---
+++ CompanyEmployees.Presentation/Controllers/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using CompanyEmployees.Presentation.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DTO;
+
+namespace CompanyEmployees.Presentation.Controllers;
+
+[Route("api/token")]
+[ApiController]
+public class TokenController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public TokenController(IServiceManager serviceManager) =>
+        _serviceManager = serviceManager;
+
+    [HttpPost("refresh")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> Refresh([FromBody] TokenDto token)
+    {
+        var tokenDto = await _serviceManager.AuthenticationService.RefreshToken(token);
+        
+        return Ok(tokenDto);
+    }
+}
